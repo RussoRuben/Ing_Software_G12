881bed3a2485929cccfa873331c6a38e9104e9ef
Rondosvalle <rubenrusso360@proton.me>
2025-11-19 17:54:53 +0100

    first commit

---
+++ src/main/java/gruppo12/bibliotecaunisa/PrimaryController.java
@@ -0,0 +1,12 @@
+package gruppo12.bibliotecaunisa;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module gruppo12.bibliotecaunisa {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens gruppo12.bibliotecaunisa to javafx.fxml;
+    exports gruppo12.bibliotecaunisa;
+}
+*/
