f4f72139f19251e8718b282f163908f643e079fe
Angelo Sica <146189@Angelo>
2025-12-10 13:59:00 +0100

    Descrizione doxygen di BibliotecaData e BibliotecaService

--- src/main/java/gruppo12/bibliotecaunisa/data/BibliotecaData.java
+++ src/main/java/gruppo12/bibliotecaunisa/data/BibliotecaData.java
@@ -4,6 +4,11 @@ import gruppo12.bibliotecaunisa.repository.LibroRepository;
 import gruppo12.bibliotecaunisa.repository.PrestitoRepository;
 import gruppo12.bibliotecaunisa.repository.StudenteRepository;
 
+/**
+ * @brief Classe che contiene i repository del sistema 
+ * @class BibliotecaData
+ * 
+ */
 public class BibliotecaData {
 
     private static final String DEFAULT_PATH = "data/stato.bin";
@@ -12,6 +17,9 @@ public class BibliotecaData {
     private final StudenteRepository studenteRepo;
     private final PrestitoRepository prestitoRepo;
 
+    /**
+     * @brief Costruttore: inizializza i repository dei libri, studenti e prestiti  
+     */
     public BibliotecaData() {
         libroRepo = new LibroRepository();
         studenteRepo = new StudenteRepository();
@@ -19,31 +27,55 @@ public class BibliotecaData {
     }
 
     // Metodi getter
+    /**
+     * @return Restituisce il repository dei libri
+     */
     public LibroRepository getLibroRepo() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+     /**
+     * @return Restituisce il repository degli studenti
+     */
     public StudenteRepository getStudenteRepo() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+     /**
+     * @return Restituisce il repository dei prestiti
+     */
     public PrestitoRepository getPrestitoRepo() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+     /**
+     * @brief Salva lo stato corrente del sistema utilizzando il percorso predefinito 
+     */
     public void salvaStato() {
         salvaStato(DEFAULT_PATH);
     }
 
     // Metodi lettura / scrittura
+     
+    /**
+     * @brief Salva lo stato corrente del sistema 
+     * @param filePath Percorso del file su cui salvare lo stato
+     */
     public void salvaStato(String filePath) {
 
     }
 
+     /**
+     * @brief Carica lo stato del sistema utilizzando il percorso predefinito
+     */
     public void caricaStato() {
         caricaStato(DEFAULT_PATH);
     }
 
+    /**
+     * @brief Carica lo stato del sistema dal file indicato 
+     * @param filePath Percorso del file da cui caricare lo stato
+     */
     @SuppressWarnings("unchecked")
     public void caricaStato(String filePath) {
 
--- src/main/java/gruppo12/bibliotecaunisa/service/BibliotecaService.java
+++ src/main/java/gruppo12/bibliotecaunisa/service/BibliotecaService.java
@@ -7,74 +7,154 @@ import gruppo12.bibliotecaunisa.model.Studente;
 import java.util.List;
 import javafx.collections.ObservableList;
 
+
+/**
+ * @brief Gestione delle operazioni su Libri, Studenti e Prestiti
+ * @class BibliotecaService 
+ */
 public class BibliotecaService {
 
     private final BibliotecaData data;
 
+    /**
+     * @brief Costruttore
+     * @param data Oggetto di tipo BibliotecaData contenente i repository
+     */
     public BibliotecaService(BibliotecaData data) {
         this.data = data;
     }
-
+    
+    /**
+     * @brief Aggiunge un nuovo libro al sistema
+     * @param libro Oggetto di tipo Libro
+     */
     public void aggiungiLibro(Libro libro) {
 
     }
 
-    public void modificaLibro(String codice, String titolo, List<String> autori, String editore, int numeroEdizione, int copie) {
+    /**
+     * @brief Modifica determinati attributi del libro
+     * @param codice 
+     * @param titolo
+     * @param autori
+     * @param editore
+     * @param numeroEdizione
+     * @param anno
+     * @param copie 
+     */
+    public void modificaLibro(String codice, String titolo, List<String> autori, String editore, int numeroEdizione, int anno, int copie) {
 
     }
-
+    
+    /**
+     * @brief Rimuovi un libro dal sistema
+     * @param libro Oggetto di tipo Libro
+     */
     public void rimuoviLibro(Libro libro) {
 
     }
-
+    
+    /**
+     * @return Restituisce una lista osservabile dei libri
+     */
     public ObservableList<Libro> getLibri() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
+    /**
+    * @brief Cerca un libro tramite codice ISBN
+    * @param codice 
+    * @return Restituisce il libro relativo al codice 
+    */
     public Libro trovaLibro(String codice) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * @brief Aggiunge uno studente al sistema
+     * @param studente oggetto di tipo Studente
+     */
     public void aggiungiStudente(Studente studente) {
 
     }
 
+    /**
+     * @brief Modifica determinati attributi di Studente
+     * @param matricola
+     * @param nome
+     * @param cognome
+     * @param email 
+     */
     public void modificaStudente(String matricola, String nome, String cognome, String email) {
 
     }
 
+    /**
+     * @brief Rimuove uno studente dal sistema
+     * @param studente Oggetto di tipo Studente
+     */
     public void rimuoviStudente(Studente studente) {
 
     }
 
+    /**
+     * @return Restituisce una lista osservabile di studenti
+     */
     public ObservableList<Studente> getStudenti() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
+    /**
+     * @brief Cerca studente in base alla Matricola
+     * @param matricola 
+     * @return Restituisce lo studente relativo alla matricola
+     */
     public Studente trovaStudente(String matricola) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * @brief Aggiungi un prestito al sistema 
+     * @param prestito oggetto di tipo Prestito
+     */
     public void aggiungiPrestito(Prestito prestito) {
 
     }
 
+    /**
+     * @brief Segna un prestito come restituito
+     * @param prestito 
+     */
     public void restituisciPrestito(Prestito prestito) {
 
     }
 
+    /**
+     * @return Restituisce una lista osservabile dei prestiti
+     */
     public ObservableList<Prestito> getPrestiti() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    /**
+     * 
+     * @return Restituisce una lista osservabile dei prestiti archiviati
+     */
     public ObservableList<Prestito> getPrestitiArchiviati() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    /**
+     * @brief Cerca prestito in base al codice
+     * @param codice univoco del Prestito
+     * @return Restituisce il prestito corrispondente al codice
+     */
     public Prestito trovaPrestito(long codice) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    /**
+     * @brief Cerca un prestito archiviato in base al codice
+     * @param codice univoco del Prestito
+     * @return Restituisce il prestito archiviato corrispondente al codice
+     */
     public Prestito trovaPrestitoArchiviato(long codice) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
